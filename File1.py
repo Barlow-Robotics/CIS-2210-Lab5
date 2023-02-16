480a1b12cdcf17037ca35a292dc6bfceacbb3628
wxk04060 <wxk04060@castleton.edu>
2023-02-16 09:33:08 -0500

    Lab1 prep fixed

--- File1.py
+++ File1.py
@@ -1,1 +1,18 @@
 
+#This function converts a number in degress C into
+# degrees F
+def convertCtoF(c):
+    f = (c * (9 / 5)) + 32
+    return f
+
+
+#this function prompts the user for a temperature
+#and prints out the converted result
+def promptAndConvert():
+    inputString = input("Enter a temperature in °C: ")
+    inputFloat = float(inputString)
+    f = convertCtoF(inputFloat)
+    print(str(inputFloat) + " is equal to " + str(f) + "°F")
+
+print("Welcome to the Temperature Converter program.")
+promptAndConvert()
